3baa4c4c387e0d38e87667f3c9d75a320574f0ca
Dmitry <dabealu@gmail.com>
2026-05-09 22:47:24 +0800

    status-line-config

--- assets/nvim/lua/plugins/lualine.lua
+++ assets/nvim/lua/plugins/lualine.lua
@@ -3,10 +3,22 @@ require("lualine").setup({
     theme = "kanagawa",
   },
   sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff" },
     lualine_c = { {
       "filename",
       symbols = { modified = "●" },
     } },
     lualine_x = { "diagnostics", "lsp_client_name" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { "filename" } },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { "location" },
   },
 })
